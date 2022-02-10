0f261b7773550af140bf24b075b54f0b0d183172
skyler.yeon <songmi.yeon@outlook.com>
2022-02-10 20:27:39 +0800

    more changes to title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `TCC Feb Workshop`,
+    title: `TCC Feb 10 Workshop`,
     author: {
       name: `Skyler Yeon`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      summary: `who lives and works in Taiwan building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
